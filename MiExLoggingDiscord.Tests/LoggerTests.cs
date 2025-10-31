343230deea508a64abdf9cb94d35aeb93d446714
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-10-31 18:41:38 +0900

    add: tests for logger construction

---
+++ MiExLoggingDiscord.Tests/LoggerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MiExLoggingDiscord.ServiceProvider;
+
+namespace MiExLoggingDiscord.Tests;
+
+public class LoggerTests : IDisposable
+{
+  public LoggerTests()
+  {
+    var config = new ConfigurationBuilder()
+      .AddUserSecrets(typeof(LoggerTests).Assembly)
+      .Build();
+    WebhookUrl = config["WebhookUrl"] ?? throw new KeyNotFoundException(nameof(WebhookUrl));
+    LoggerConstructor = LoggerFactory.Create(builder => { builder.AddDiscordLogger(WebhookUrl); });
+  }
+
+  private string WebhookUrl { get; }
+  private ILoggerFactory LoggerConstructor { get; }
+
+  public void Dispose()
+  {
+    LoggerConstructor.Dispose();
+    GC.SuppressFinalize(this);
+  }
+
+  [Test]
+  public void CanConstructLogger()
+  {
+    Assert.DoesNotThrow(() => { _ = LoggerConstructor.CreateLogger("Default"); });
+  }
+
+  [Test]
+  public void CanConstructGenericLogger()
+  {
+    Assert.DoesNotThrow(() => { _ = LoggerConstructor.CreateLogger<object>(); });
+  }
+}
