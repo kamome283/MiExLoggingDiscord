aa1ecde05b4e128e4212a240610abec954c0a986
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-10-31 21:17:23 +0900

    add: tests for logging itself

--- MiExLoggingDiscord.Tests/LoggerTests.cs
+++ MiExLoggingDiscord.Tests/LoggerTests.cs
@@ -11,8 +11,15 @@ public class LoggerTests : IDisposable
       .AddUserSecrets(typeof(LoggerTests).Assembly)
       .Build();
     WebhookUrl = config["WebhookUrl"] ?? throw new KeyNotFoundException(nameof(WebhookUrl));
-    LoggerConstructor = LoggerFactory.Create(builder => { builder.AddDiscordLogger(WebhookUrl); });
+    LoggerConstructor = LoggerFactory.Create(builder =>
+    {
+      builder
+        .AddDiscordLogger(WebhookUrl)
+        .SetMinimumLevel(LogLevel.Trace);
+    });
   }
+
+  private Task Throttle => Task.Delay(1000);
 
   private string WebhookUrl { get; }
   private ILoggerFactory LoggerConstructor { get; }
@@ -24,14 +31,71 @@ public class LoggerTests : IDisposable
   }
 
   [Test]
-  public void CanConstructLogger()
+  [Category("WebRequest")]
+  public async Task LoggerWorksAsDesired()
   {
-    Assert.DoesNotThrow(() => { _ = LoggerConstructor.CreateLogger("Default"); });
+    var logger = LoggerConstructor.CreateLogger("Default");
+    logger.LogTrace("log at {Time}", DateTimeOffset.Now);
+    await Throttle;
   }
 
   [Test]
-  public void CanConstructGenericLogger()
+  [Category("WebRequest")]
+  public async Task GenericLoggerWorksAsDesired()
   {
-    Assert.DoesNotThrow(() => { _ = LoggerConstructor.CreateLogger<object>(); });
+    var logger = LoggerConstructor.CreateLogger<object>();
+    logger.LogTrace("log from generic logger at {Time}", DateTimeOffset.Now);
+    await Throttle;
+  }
+
+  [Test]
+  [Category("WebRequest")]
+  public async Task LoggingWithScopeWorksAsDesired()
+  {
+    var logger = LoggerConstructor.CreateLogger("With Scope");
+    using var _ = logger.BeginScope("Scoped");
+    logger.LogDebug("Log with scope at {Time}", DateTimeOffset.Now);
+    await Throttle;
+  }
+
+  [Test]
+  [Category("WebRequest")]
+  public async Task LoggingWithEventIdWorksAsDesired()
+  {
+    var logger = LoggerConstructor.CreateLogger("With Event Id");
+    logger.LogInformation(new EventId(1), "log with event id at {Time}", DateTimeOffset.Now);
+    await Throttle;
+  }
+
+  [Test]
+  [Category("WebRequest")]
+  public async Task LoggingWithExceptionWorksAsDesired()
+  {
+    var logger = LoggerConstructor.CreateLogger("With Exception");
+    var ex = new InvalidOperationException("Some Exception");
+    logger.LogWarning(ex, "log with exception at {Time}", DateTimeOffset.Now);
+    await Throttle;
+  }
+
+  [Test]
+  [Category("WebRequest")]
+  public async Task LoggingWithNestedExceptionWorksAsDesired()
+  {
+    var logger = LoggerConstructor.CreateLogger("With Nested Exception");
+    var ex = new InvalidOperationException("Nested Exception", new KeyNotFoundException("Some Key"));
+    logger.LogError(ex, "log with nested exception at {Time}", DateTimeOffset.Now);
+    await Throttle;
+  }
+
+  [Test]
+  [Category("WebRequest")]
+  public async Task FullSpecLoggingWorksAsDesired()
+  {
+    var logger = LoggerConstructor.CreateLogger<object>();
+    using var outerScope = logger.BeginScope("outer");
+    using var innerScope = logger.BeginScope("inner");
+    var ex = new InvalidOperationException("Nested Exception", new KeyNotFoundException("Some Key"));
+    logger.LogCritical(new EventId(1, "Full"), ex, "Full spec logging at {Time}", DateTimeOffset.Now);
+    await Throttle;
   }
 }
